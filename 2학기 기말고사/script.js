2ef6953a99b829324067898170122910cda3a367
alexchoi1019 <162537786+alexchoi1019@users.noreply.github.com>
2024-12-20 10:02:27 +0900

    Delete 2학기 기말고사 directory

--- 2학기 기말고사/script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("startBtn").addEventListener("click", () => {
-    const nickname = document.getElementById("nickname").value;
-    if (nickname) {
-      window.location.href = "task.html";
-    } else {
-      alert("닉네임을 입력하세요.");
-    }
-  });
-  
